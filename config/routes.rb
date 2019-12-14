6d33379386b3c0838bb1779da7f4e4d611a17742
DevelopingKuma <taihei2009@hotmail.co.jp>
2019-12-14 12:25:46 +0900

    アプリ全体の大枠をつくりました。いうてまだidとかやってないですが、基本はこの枠かと。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup/' => "home#signup"
+  get '/personal/' => "home#personal"
+  get '/personal/assets' => "home#assets"
+  get '/people' => "home#people"
+  get '/people/info' => "home#info"
+  get '/trade/' => "home#trade"
+  #personalとかtradeのところはidを振ります。ひかるんがDBつくってくれたら反映します。
+
 end
